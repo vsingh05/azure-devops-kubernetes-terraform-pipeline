b69947e5376b3aa46b371f1047dc2faa72205bf3
Varinder Singh <vsingh051986@gmail.com>
2020-10-20 13:56:04 +0200

    Update main.tf

--- configuration/iaac/azure/backend/main.tf
+++ configuration/iaac/azure/backend/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~> 1.42"
+  version = "~> 2.0"
   client_id       = var.client_id   # ENVIRONMENT VARIABLE
   client_secret   = var.client_secret # ENVIRONMENT VARIABLE
   subscription_id = var.subscription_id
